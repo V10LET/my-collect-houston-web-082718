e0d20937693a65392d8afc70e6afa091e157bf69
v10let <violet.wmoon@gmail.com>
2018-08-27 20:56:58 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+# friends = ['giraffie', 'kitten', 'monkey', 'puggles']
+names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(array)
+	first_name = []
+	i = 0
+	while i < array.length
+		first_name << yield(array[i])
+		i += 1
+	end
+	first_name
+end
+
+my_collect(names) { |name| name.split(" ").first }
 
